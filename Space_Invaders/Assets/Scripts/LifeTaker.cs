561cde1ca23b827be2c1db563bbac526396aeacf
Dom338 <domenicopetrillod@gmail.com>
2026-02-26 10:58:13 +0100

    life quick fix

--- Space_Invaders/Assets/Scripts/LifeTaker.cs
+++ Space_Invaders/Assets/Scripts/LifeTaker.cs
@@ -1,16 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+
 
 public class LifeTaker : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+  public int takeDamage = 1;
+  public bool skipinvincible = false;
+  public List<string> tags;
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    bool tagExist = false;
+
+    foreach (string tag in tags)
     {
-        
+      if (collision.CompareTag(tag))
+      {
+        tagExist = true;
+        break;
+      }
     }
 
-    // Update is called once per frame
-    void Update()
+    if (tagExist)
     {
-        
+      collision.GetComponent<LifeController>()?.damage(takeDamage, skipinvincible);
     }
+
+  }
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
